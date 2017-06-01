09ec45887dc60207636ef1a60083138ebd58a6b1
Mirco <ablx@posteo.de>
2017-06-01 13:20:38 +0200

    Basic anonymization

--- src/main/java/ml/anon/model/anonymization/Anonymization.java
+++ src/main/java/ml/anon/model/anonymization/Anonymization.java
@@ -1,11 +1,13 @@
 package ml.anon.model.anonymization;
 
+import lombok.Builder;
 import lombok.Data;
 
 /**
  * Created by mirco on 20.05.17.
  */
 @Data
+@Builder
 public class Anonymization {
 
     private long id;
