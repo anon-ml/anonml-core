52a3112c27a891511d9048127d973a08898322a9
Mirco <ablx@posteo.de>
2017-06-11 16:26:03 +0200

    RegEx integration to frontend

--- src/main/java/ml/anon/model/docmgmt/DocumentAccess.java
+++ src/main/java/ml/anon/model/docmgmt/DocumentAccess.java
@@ -2,8 +2,13 @@ package ml.anon.model.docmgmt;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
+import ml.anon.model.anonymization.Anonymization;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by mirco on 11.06.17.
@@ -20,5 +25,13 @@ public class DocumentAccess {
         String url = IP + "/document/" + id;
         log.info(url);
         return restTemplate.getForEntity(url, Document.class, new Object[]{});
+    }
+
+    public Document updateDocument(String id, List<Anonymization> anon) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("anonymizations", anon);
+        restTemplate.put(IP + "/document/" + id, Document.builder().anonymizations(anon).id(id).originalFileType(FileType.PDF).build(), params);
+
+        return getDocument(id).getBody();
     }
 }
