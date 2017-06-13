ce6335a845eff6a1147525add615a066ad2a053a
Mirco <ablx@posteo.de>
2017-06-13 21:22:30 +0200

    Refactoring, enables more powerfull rules

---
+++ src/main/java/ml/anon/annotation/ReplacementGenerator.java
@@ -0,0 +1,13 @@
+package ml.anon.annotation;
+
+import ml.anon.model.anonymization.Label;
+
+/**
+ * Created by mirco on 13.06.17.
+ */
+public class ReplacementGenerator {
+
+    public String generateReplacement(String original, Label label) {
+        return "##############";
+    }
+}
