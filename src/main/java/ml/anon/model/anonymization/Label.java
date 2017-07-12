1f1f741dd450ef78aa862865ce75a4f38f8e7648
Mirco <ablx@posteo.de>
2017-07-12 20:39:28 +0200

    Fixed broken labeling

--- src/main/java/ml/anon/model/anonymization/Label.java
+++ src/main/java/ml/anon/model/anonymization/Label.java
@@ -7,7 +7,16 @@ import java.util.List;
  * Created by mirco on 20.05.17.
  */
 public enum Label {
-    PERSON, LOCATION, ORGANIZATION, MISC, LICENCE_PLATE, E_MAIL, TELEPHONE_NUMBER, URL, IP, BIRTHDATE, IBAN, CLIENT_NUMBER;
+    PERSON, LOCATION, ORGANIZATION, MISC, LICENCE_PLATE, E_MAIL, TELEPHONE_NUMBER, URL, IP, BIRTHDATE, IBAN, CLIENT_NUMBER, UNKNOWN;
+
+    public static Label getOrDefault(String label, Label def) {
+        try {
+            return Label.valueOf(label);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return def;
+        }
+    }
 
     public static List<Label> getAll() {
         return Arrays.asList(Label.values());
